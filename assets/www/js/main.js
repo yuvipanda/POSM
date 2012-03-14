3f515f034c3f8092440f10d11abe95aaf88935d4
YuviPanda <yuvipanda@gmail.com>
2012-03-14 20:11:17 +0530

    Revert "CSS for flexbox display :D"
    FlexBox causing issues with iOS orientationchange
    
    This reverts commit bb13739010d29200c40b5127440fb959bb90f3b6.

--- assets/www/js/main.js
+++ assets/www/js/main.js
@@ -124,7 +124,21 @@ document.addEventListener("mobileinit", function() {
     $.mobile.page.prototype.options.backBtnText = "";
 }, true);
 
+function resizeContentArea() {
+    var content, contentHeight, footer, header, viewportHeight;
+    window.scroll(0, 0);
+    header = $(":jqmData(role='header'):visible");
+    footer = $(":jqmData(role='footer'):visible");
+    content = $(":jqmData(role='content'):visible");
+    viewportHeight = $(window).height();
+    contentHeight = viewportHeight - header.outerHeight() - footer.outerHeight();
+    $("article:jqmData(role='content')").first().height(contentHeight);
+    return $("#map").height(contentHeight);
+  };
+
 function init() {
+    $(window).bind('orientationchange resize', resizeContentArea);
+    $("#map-page").bind('pageshow', resizeContentArea);
     map = new L.Map('map');
 
     osmTiles = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -139,6 +153,7 @@ function init() {
     map.on('locationfound', function(pos) {
         curPosManager.showPosition(pos.latlng, pos.accuracy);
     });
+    resizeContentArea();
 
     map.on('zoomend', function(ev) {
         if(map.getZoom() >= 3 && !autoPOI) {
